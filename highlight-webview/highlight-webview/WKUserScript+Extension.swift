4b40db18ff691f9f35a4513fd6a3667c941e7925
= <longvu@Longs-MacBook-Pro-2.local>
2020-12-11 10:20:13 +0700

    add extentions

---
+++ highlight-webview/highlight-webview/WKUserScript+Extension.swift
@@ -0,0 +1,56 @@
+//
+//  WKUserScript+Extension.swift
+//  highlight-webview
+//
+//  Created by Long Vu on 11/12/2020.
+//
+
+import WebKit
+
+extension WKUserScript {
+    static func injectCSSBase64(_ data: String) -> WKUserScript {
+        let script = """
+            javascript:(function() {
+            var parent = document.getElementsByTagName('head').item(0);
+            var style = document.createElement('style');
+            style.type = 'text/css';
+            style.innerHTML = window.atob('\(data)');
+            parent.appendChild(style)})()
+        """
+        return WKUserScript(
+            source: script,
+            injectionTime: .atDocumentEnd,
+            forMainFrameOnly: false
+        )
+    }
+    static func injectJSBase64(_ data: String) -> WKUserScript {
+        let script = """
+            javascript:(function() {
+            var parent = document.getElementsByTagName('head').item(0);
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.innerHTML = window.atob('\(data)');
+            parent.appendChild(script)})()
+        """
+        return WKUserScript(
+            source: script,
+            injectionTime: .atDocumentEnd,
+            forMainFrameOnly: false
+        )
+    }
+    
+    static func fromFile(_ filename: String, type: String) -> WKUserScript {
+        guard
+            let path = Bundle.main.path(forResource: filename, ofType: type),
+            let base64 = try? String(contentsOfFile: path).toBase64String()
+            else {
+            return WKUserScript()
+        }
+        var script = WKUserScript.injectJSBase64(base64)
+        if type == "css" {
+            script = WKUserScript.injectCSSBase64(base64)
+        }
+        return script
+    }
+}
+
