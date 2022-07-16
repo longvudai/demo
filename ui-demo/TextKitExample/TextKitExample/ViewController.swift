94e3303130700e8ca7284095a1b2cbc3ec6c8183
Long Vu <long.vudai01@gmail.com>
2022-07-16 13:30:25 +0700

    add initial TextKitExample

---
+++ ui-demo/TextKitExample/TextKitExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextKitExample
+//
+//  Created by Long Vu on 16/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
