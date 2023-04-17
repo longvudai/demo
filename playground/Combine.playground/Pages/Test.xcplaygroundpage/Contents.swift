def92219c7b11d7a5f405a9630ce1049dfc5c534
Long Vu <long.vudai01@gmail.com>
2023-04-17 23:27:30 +0700

    test merge many

--- playground/Combine.playground/Pages/Test.xcplaygroundpage/Contents.swift
+++ playground/Combine.playground/Pages/Test.xcplaygroundpage/Contents.swift
@@ -6,16 +6,42 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 var cancellableSet: Set<AnyCancellable> = []
 
-let future = Future<Int, Never> { promise in
-    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-        let sum = (0...1000).reduce(0, +)
-        promise(.success(sum))
-    }
-}
-
+//let future = Future<Int, Never> { promise in
+//    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//        let sum = (0...1000).reduce(0, +)
+//        promise(.success(sum))
+//    }
+//}
+//
 //future
 //    .receive(on: DispatchQueue.main)
 //    .sink { sum in
 //        print("sum", sum)
 //    }
 //    .store(in: &cancellableSet)
+
+let subject: PassthroughSubject<[Int], Never> = .init()
+
+subject
+    .map { ints -> [AnyPublisher<Int, Never>] in
+        let result: [AnyPublisher<Int, Never>] = ints.map { value in
+            return [value, value + 1].publisher
+//                .prefix(1)
+                .eraseToAnyPublisher()
+        }
+        return result
+    }
+    .flatMap { streams in
+        return Publishers.MergeMany(streams)
+            .collect()
+            .eraseToAnyPublisher()
+    }
+    .receive(on: DispatchQueue.main)
+    .sink { v in
+        print("value", v)
+    }
+    .store(in: &cancellableSet)
+
+
+subject.send([1, 2])
+subject.send([1, 2, 3])
--- playground/Combine.playground/Pages/URLRequest.xcplaygroundpage/Contents.swift
+++ playground/Combine.playground/Pages/URLRequest.xcplaygroundpage/Contents.swift
@@ -4,6 +4,7 @@ import Foundation
 import Combine
 
 var greeting = "Hello, playground"
-var a = URLSession.shared.dataTaskPublisher(for: <#T##URL#>)
+
+
 
 //: [Next](@next)
